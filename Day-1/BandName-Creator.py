fc8a8a613a6ba4d70ccbac04ecf43752e8e7bb1b
Aayush Sharma <aayusharma1401@gmail.com>
2021-11-02 10:51:50 +0530

    Create BandName-Creator.py

---
+++ Day-1/BandName-Creator.py
@@ -0,0 +1,5 @@
+print("Welcome to band name creator")
+# BAND NAME CREATOR
+city=input("Enter Your City Name\n")
+name=input("Enter Your Name\n")
+print("Your band name could be " + city + " " + name)
